f994f28a1fae843d84d2766e0d5d23dc291c559c
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-04-29 10:53:18 +0200

    UserService static

---
+++ SkolefotograferneSemesterProjekt/Interfaces/ISchoolAdminService.cs
@@ -0,0 +1,13 @@
+﻿using SkolefotograferneSemesterProjekt.Models;
+
+namespace SkolefotograferneSemesterProjekt.Interfaces
+{
+    public interface ISchoolAdminService
+    {
+        Task Add(SchoolAdmin schoolAdmin);
+        Task<SchoolAdmin> GetByID(int id);
+        Task<List<SchoolAdmin>> GetAll();
+        Task Update(SchoolAdmin schoolAdmin);
+        
+    }
+}
---
+++ SkolefotograferneSemesterProjekt/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using SkolefotograferneSemesterProjekt.Interfaces;
+using SkolefotograferneSemesterProjekt.Models;
+
+namespace SkolefotograferneSemesterProjekt.Services
+{
+    public class UserService : Connection,IUserService
+    {
+        public async Task<int> Add(SqlConnection conn, User user)
+        {
+            var cmd = new SqlCommand(@"
+                INSERT INTO Users (Email, Password, Role)
+                VALUES (@Email, @Password, @Role);
+                SELECT SCOPE_IDENTITY();
+            ", conn);
+
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@Role", user.Role);
+
+            var result = await cmd.ExecuteScalarAsync();
+            return Convert.ToInt32(result);
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User VerifyUser(string mail, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
