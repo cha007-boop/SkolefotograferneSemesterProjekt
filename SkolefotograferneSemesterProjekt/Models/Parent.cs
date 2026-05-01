66b2e5f1064ab35442d47b1313c0ec1c06cec195
Modjo1 <rebeccalauruhn@gmail.com>
2026-05-01 12:35:59 +0200

    Have added som sorting and filtering to parents

--- SkolefotograferneSemesterProjekt/Models/Parent.cs
+++ SkolefotograferneSemesterProjekt/Models/Parent.cs
@@ -11,5 +11,7 @@
         {
             Role = UserRole.Parent;
         }
+
+        
     }
 }
--- SkolefotograferneSemesterProjekt/Pages/Parents/Index.cshtml.cs
+++ SkolefotograferneSemesterProjekt/Pages/Parents/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SkolefotograferneSemesterProjekt.Helpers.Sorting;
 using SkolefotograferneSemesterProjekt.Interfaces;
 using SkolefotograferneSemesterProjekt.Models;
 using System.Threading.Tasks;
@@ -11,20 +12,68 @@ namespace SkolefotograferneSemesterProjekt.Pages.Parents
         private IParentServices _parentService;
         public List<Parent> Parents { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SortBy { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SortOrder { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Filter { get; set; }
+
+        
+
         public IndexModel(IParentServices parentservice)
         {
             _parentService = parentservice;
+            SortOrder = "asc";
         }
         public async Task OnGet()
         {
             try
             {
-                Parents = await _parentService.GetAllParents();
+                if (!string.IsNullOrEmpty(Filter))
+                {
+                    Parents = await _parentService.FilterParents(Filter);
+                }else
+                    Parents = await _parentService.GetAllParents();
+
+                Parents = SortParents(Parents);
             }
             catch (Exception ex)
             {
                 ViewData["Errormessage"] = ex.Message;
             }
+        }
+
+        public string Toggle(string column)
+        {
+            if (SortOrder == "asc" && SortBy == column)
+            {
+                return "desc";
+            }
+            return "asc";
+        }
+
+        public List<Parent> SortParents(List<Parent> parents)
+        {
+            switch (SortBy)
+            {
+                case "FirstName":
+                    parents.Sort(new ParentsCompareName());
+                    break;
+                case "Surname":
+                    parents.Sort(new ParentsCompareSurname());
+                    break;
+                case "PhoneNumber":
+                    parents.Sort(new ParentsComparePhoneNumber());
+                    break;
+                default:
+                    break;
+
+            }
+            if (SortOrder != "asc") parents.Reverse();
+            return parents;
         }
 
 
