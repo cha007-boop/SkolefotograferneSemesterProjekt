475d2e5a813e69ec1dc5a1c6027430790b2484b3
Alexander Larsen <all005@edu.zealand.dk>
2026-05-01 13:46:54 +0200

    benis

---
+++ SkolefotograferneSemesterProjekt/Helpers/SchoolYearCalc.cs
@@ -0,0 +1,15 @@
+﻿namespace SkolefotograferneSemesterProjekt.Helpers
+{
+    public static class SchoolYearCalc
+    {
+        public static string GetSchoolYear()
+        {
+            int year = DateTime.Now.Year;
+            if (DateTime.Now.Month < 8)
+            {
+                year -= 1;
+            }
+            return year.ToString();
+        }
+    }
+}
