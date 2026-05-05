74c6997616ef714c4bb0524d521e78e13055ed0a
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-05-05 09:56:32 +0200

    photoservice

---
+++ SkolefotograferneSemesterProjekt/Interfaces/IPhotoService.cs
@@ -0,0 +1,15 @@
+﻿using SkolefotograferneSemesterProjekt.Models;
+
+namespace SkolefotograferneSemesterProjekt.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task Add(Photo photo);
+        Task<Photo> GetByFilename(string filename);
+        Task<List<Photo>> GetAll();
+        Task<List<Photo>> GetClassPhotosByClass(SchoolClass schoolClass);
+        Task<List<Photo>> GetPortraitsByStudent(Student student);
+        Task RemovePhoto(Photo photo);
+
+    }
+}
