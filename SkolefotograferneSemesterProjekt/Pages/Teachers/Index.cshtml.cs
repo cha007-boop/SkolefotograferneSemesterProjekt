258f0c2d3474191f34812f23a4fa871496c69234
Neozt001 <neo001@edu.zealand.dk>
2026-05-05 19:02:09 +0200

    Fix (Teachers.Index): restricted access to logged-in users and sysAdmin

--- SkolefotograferneSemesterProjekt/Pages/Teachers/Index.cshtml.cs
+++ SkolefotograferneSemesterProjekt/Pages/Teachers/Index.cshtml.cs
@@ -9,15 +9,32 @@ namespace SkolefotograferneSemesterProjekt.Pages.Teachers
     {
         public ITeacherService _repo;
         public List<Teacher> TeacherList { get; set; }
+        public Teacher TheTeacher { get; set; }
+        public int? UserID { get; set; }
+        public bool IsUser { get; set; }
 
         public IndexModel(ITeacherService repo)
         {
             _repo = repo;
+            
         }
 
         public async Task OnGet()
         {
             TeacherList = await _repo.GetAll();
+
+            UserID = HttpContext.Session.GetInt32("ID");
+            if (UserID != null)
+            {
+                Teacher t = new Teacher { ID = (int)UserID };
+                t = TeacherList.Find((t) => t.ID == UserID)!;
+                if (t != null)
+                {
+                    IsUser = true;
+                    TheTeacher = t;
+                }
+            }
+            
         }
     }
 }
