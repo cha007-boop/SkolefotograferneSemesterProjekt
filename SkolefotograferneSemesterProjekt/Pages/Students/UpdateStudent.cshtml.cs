c304f8c12f5b5d1aeb044bf9a64ed474f2a39bf8
Alexander Larsen <all005@edu.zealand.dk>
2026-05-05 09:53:22 +0200

    s

--- SkolefotograferneSemesterProjekt/Pages/Students/UpdateStudent.cshtml.cs
+++ SkolefotograferneSemesterProjekt/Pages/Students/UpdateStudent.cshtml.cs
@@ -1,12 +1,47 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SkolefotograferneSemesterProjekt.Interfaces;
+using SkolefotograferneSemesterProjekt.Models;
+using SkolefotograferneSemesterProjekt.Services;
 
 namespace SkolefotograferneSemesterProjekt.Pages.Students
 {
     public class UpdateStudentModel : PageModel
     {
-        public void OnGet()
+        #region Instance fields
+        private IStudentService _studentService;
+        #endregion
+        #region Properties
+        [BindProperty]
+        public Student NewStudent { get; set; }
+        #endregion
+        #region Constructor
+        public UpdateStudentModel(IStudentService studentService)
         {
+            _studentService = studentService;
         }
+        #endregion
+        #region Methods
+        public async Task OnGet(int id)
+        {
+            try
+            {
+                if (HttpContext.Session.GetInt32("Role") == 0)
+                {
+
+                }
+                NewStudent = await _studentService.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                ViewData["ErrorMessage"] = ex.Message;
+            }
+        }
+        public void OnPost()
+        {
+
+        }
+        #endregion
     }
 }
