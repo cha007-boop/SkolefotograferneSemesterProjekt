0b9243dd82ced9259e2b31b8e8cafd2f936aff70
Neozt001 <neo001@edu.zealand.dk>
2026-05-08 11:40:08 +0200

    Few edits (Bookings)

---
+++ SkolefotograferneSemesterProjekt/Pages/Bookings/UpdateBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SkolefotograferneSemesterProjekt.Pages.Bookings
+{
+    public class UpdateBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
