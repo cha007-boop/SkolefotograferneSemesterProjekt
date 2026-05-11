1937b0886c11c0f16f1823b38c53d8a724add9d1
Alexander Larsen <all005@edu.zealand.dk>
2026-05-11 13:20:35 +0200

    -Students/CreateStudent (updated an if statement) -Students/UpdateStudent(added html for the page) (added instance fields, properties, and OnPostUpdate method, updated the OnGet method, updated constructor)

--- SkolefotograferneSemesterProjekt/Pages/Students/UpdateStudent.cshtml.cs
+++ SkolefotograferneSemesterProjekt/Pages/Students/UpdateStudent.cshtml.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SkolefotograferneSemesterProjekt.Helpers;
 using SkolefotograferneSemesterProjekt.Interfaces;
 using SkolefotograferneSemesterProjekt.Models;
 using SkolefotograferneSemesterProjekt.Services;
@@ -11,15 +13,28 @@ namespace SkolefotograferneSemesterProjekt.Pages.Students
     {
         #region Instance fields
         private IStudentService _studentService;
+        private ISchoolService _schoolService;
+        private ISchoolClassService _schoolClassService;
+        private IParentServices _parentServices;
         #endregion
         #region Properties
         [BindProperty]
         public Student NewStudent { get; set; }
+        [BindProperty]
+        public string SchoolID { get; set; }
+        [BindProperty]
+        public int ClassGrade { get; set; }
+        [BindProperty]
+        public string ClassLetter { get; set; }
+        public IEnumerable<SelectListItem> Schools { get; set; }
         #endregion
         #region Constructor
-        public UpdateStudentModel(IStudentService studentService)
+        public UpdateStudentModel(IStudentService studentService, ISchoolService schoolService, ISchoolClassService schoolClassService, IParentServices parentServices)
         {
             _studentService = studentService;
+            _schoolService = schoolService;
+            _schoolClassService = schoolClassService;
+            _parentServices = parentServices;
         }
         #endregion
         #region Methods
@@ -38,11 +53,42 @@ namespace SkolefotograferneSemesterProjekt.Pages.Students
                 ViewData["ErrorMessage"] = ex.Message;
                 return RedirectToPage("/Index");
             }
+            List<School> schools = await _schoolService.GetAll();
+            Schools = schools.Select(s => new SelectListItem
+            {
+                Value = Convert.ToString(s.ID),
+                Text = $"{s.Name} - {s.Street} {s.ZipCode}"
+            });
             return Page();
         }
-        public void OnPost()
+        public async Task<IActionResult> OnPostUpdate()
         {
-
+            ModelState.Clear();
+            TryValidateModel(ClassGrade);
+            try
+            {
+                if (ClassGrade > 10 || ClassGrade < 0)
+                {
+                    ModelState.AddModelError("ClassGrade", "Invalid Grade");
+                    return Page();
+                }
+                string year = SchoolYearCalc.GetSchoolYear();
+                NewStudent.TheSchoolClass = await _schoolClassService.SearchSchoolClass(Convert.ToInt32(SchoolID), ClassGrade, ClassLetter, year) ?? throw new ArgumentException();
+                NewStudent.TheSchool = await _schoolService.GetById(Convert.ToInt32(SchoolID));
+                NewStudent.TheParent = await _parentServices.SearchParent((int)HttpContext.Session.GetInt32("ID"));
+                await _studentService.Update(NewStudent);
+            }
+            catch (ArgumentException)
+            {
+                ModelState.AddModelError("ClassGrade", "Class doesn't exist");
+                //await OnGet();
+                return Page();
+            }
+            catch (Exception exc)
+            {
+                ViewData["ErrorMessage"] = exc;
+            }
+            return RedirectToPage("Users/Profile");
         }
         #endregion
     }
