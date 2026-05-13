794a05154d1a2410daffb12c3c25e71b0dd9e192
Alexander Larsen <all005@edu.zealand.dk>
2026-05-13 09:24:40 +0200

    -Models/User (added required/stringlength attributes to password, with a minimum length 6)

--- SkolefotograferneSemesterProjekt/Models/User.cs
+++ SkolefotograferneSemesterProjekt/Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace SkolefotograferneSemesterProjekt.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkolefotograferneSemesterProjekt.Models
 {
     public class User
     {
         public int ID { get; set; }
         public string Email { get; set; }
+        [Required]
+        [StringLength(maximumLength: 255, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
         public string Password { get; set; }
         public UserRole Role { get; set; }
         public User()
