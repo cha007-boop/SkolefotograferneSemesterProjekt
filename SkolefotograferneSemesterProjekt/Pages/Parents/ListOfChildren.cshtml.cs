66cce5191576c69ccf700184de4619aae9539000
Alexander Larsen <all005@edu.zealand.dk>
2026-05-26 12:34:48 +0200

    -mostly minor changes to spelling -CreateParent removed unused references

--- SkolefotograferneSemesterProjekt/Pages/Parents/ListOfChildren.cshtml.cs
+++ SkolefotograferneSemesterProjekt/Pages/Parents/ListOfChildren.cshtml.cs
@@ -8,16 +8,12 @@ namespace SkolefotograferneSemesterProjekt.Pages.Parents
     public class ListOfChildrenModel : PageModel
     {
         private IStudentService _studentService;
-        //private IParentServices _parentService;
         [BindProperty]
         public List<Student> Students { get; set; }
 
-        //public Parent TheParent { get; set; }
-
-        public ListOfChildrenModel(IStudentService studentService/*, IParentServices parentService*/)
+        public ListOfChildrenModel(IStudentService studentService)
         {
             _studentService = studentService;
-            //_parentService = parentService;
         }
         public async Task<IActionResult> OnGetAsync()
         {
@@ -27,7 +23,6 @@ namespace SkolefotograferneSemesterProjekt.Pages.Parents
                 {
                     throw new AccessViolationException();
                 }
-                //TheParent = await _parentService.SearchParent((int)HttpContext.Session.GetInt32("ID"));
                 Students = await _studentService.GetAllByParent((int)HttpContext.Session.GetInt32("ID"));
             }
             catch (AccessViolationException avx)
